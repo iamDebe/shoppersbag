3b2e51ad2ebf132c244d0bf491ec3a6ddc9fb8fb
srv1 <nnebuchi.j.osigbo@gmail.com>
2022-11-18 04:39:59 +0100

    ladning page ready to push

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,48 +1,10 @@
-const forShoppers = document.getElementById("forShoppers");
-const forMalls = document.getElementById("forMalls");
-const forEco = document.getElementById("forEco");
-const forBrands = document.getElementById("forBrands");
-    // console.log([forShoppers, forMalls, forEco, forBrands])
-    // console.log(forShoppers)
+function showUserCategoryDetail(event){
+    document.querySelectorAll(".users-category-detail").forEach(function(userCat){
+        userCat.classList.remove('showing')
+    })
+    // const userCategorySelector = document.querySelector('#users-categories');
 
-
-
-const shoppersCateg = document.getElementById("shoppers");
-const mallsCateg = document.getElementById("malls");
-const ecoFriendlyCateg = document.getElementById("ecoFriendly");
-const brandsCateg = document.getElementById("brands");
-    // console.log([shoppersCateg, mallsCateg, ecoFriendlyCateg, brandsCateg])
-    // console.log(shoppersCateg)
-
-    const hideShoppers = ()=>{
-        shoppersCateg.style.display = "none"
-        console.log(shoppersCateg)
-    }
-
-
-    shoppersCategList = shoppersCateg.classList
-    // mallCategList = mallsCateg.classList
-    // ecoFriendlyCategList = ecoFriendlyCateg.classList
-    // brandsCategList = brandsCateg.classList
-    
-    
-    // forShoppers.addEventListener("click", ()=>{
-    //     shoppersCategList.add("d-none")
-        
-    //     }
-    // )
-    // forMalls.addEventListener("click", ()=>{
-    //     mallCategList.replace("d-none", "d-block")
-    //     shoppersCategList.add("d-none")
-    //     }
-    // )
-    // forEco.addEventListener("click", ()=>{
-    //     ecoFriendlyCategList.replace("d-none", "d-block")
-    //     shoppersCategList.add("d-none")
-    //     }
-    // )
-    // forBrands.addEventListener("click", ()=>{
-    //     brandsCategList.remove("d-none", "d-block")
-    //     shoppersCategList.add("d-none")
-    //     }
-    // )
+    const targetId = event.target.value;
+    console.log(targetId)
+    document.querySelector("#"+targetId).classList.add('showing');
+}
